120a8d589f59e47d61679c79de8f2e46f27fdb06
Kevin Nicholas <thjin_wen_chiang@Kevins-MacBook-Air.local>
2020-06-16 09:28:14 +0700

    fqwjfklqwjklfqw

--- AdminApp/settings.py
+++ AdminApp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wpiqgnxic50!k((fc^tvex5-mh))f9&-5d-b+i_!rmcu560ets'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mari-bermain-matematika.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'AdminApp.urls'
@@ -118,5 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn admin-app.wsgi --log-file -
